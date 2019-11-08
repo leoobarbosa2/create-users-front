60ea33a81357a960bda423fb00e4fd8c77d1b2e6
Leonardo Barbosa <leonardo.pb@icloud.com>
2019-11-07 21:38:40 -0300

    Iniciando frontend

---
+++ src/styles/global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    background: #eee;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  /* body, html, #root {
+    height: 100%;
+  } */
+`;
